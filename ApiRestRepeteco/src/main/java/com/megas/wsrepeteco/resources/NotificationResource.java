d930d7209e70f365cfa19620e0d19972c7939f75
luis.o.oliveira <luis.o.oliveira@accenture.com>
2018-04-28 19:54:40 -0300

    notification delete

--- ApiRestRepeteco/src/main/java/com/megas/wsrepeteco/resources/NotificationResource.java
+++ ApiRestRepeteco/src/main/java/com/megas/wsrepeteco/resources/NotificationResource.java
@@ -1,22 +1,42 @@
 package com.megas.wsrepeteco.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.megas.wsrepeteco.domain.Notification;
+import com.megas.wsrepeteco.services.NotificationService;
 
 
 @RestController
 @RequestMapping("/wsrepeteco/notification")
 public class NotificationResource {
-	
-
-	
-//	@Autowired
-//	private  UsersService usersService;
-	
-//	@Autowired
-//	private  Notification notification;
 		
+	@Autowired
+	private NotificationService notificationService;
+	
+	
+	@CrossOrigin
+	@RequestMapping(value = "status/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> status(@RequestBody Notification notification, @PathVariable("id") Long id) {
+		notification.setId(id);
+		notificationService.status(notification);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@CrossOrigin
+	@RequestMapping(value = "delete/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> deletar(@PathVariable("id") Long id) {
+		notificationService.deletar(id);
 
+		return ResponseEntity.noContent().build();
+	}
 	
 	
 }
--- ApiRestRepeteco/src/main/java/com/megas/wsrepeteco/services/NotificationService.java
+++ ApiRestRepeteco/src/main/java/com/megas/wsrepeteco/services/NotificationService.java
@@ -1,9 +1,17 @@
 package com.megas.wsrepeteco.services;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
 
+import com.megas.wsrepeteco.domain.Notification;
 import com.megas.wsrepeteco.repository.NotificationRepository;
+import com.megas.wsrepeteco.services.exceptions.OptionNaoEncontradoException;
+import com.megas.wsrepeteco.services.exceptions.UsersNaoEncontradoException;
 
+@Service
 public class NotificationService {
 	
 
@@ -11,7 +19,49 @@ public class NotificationService {
 	@Autowired
 	private NotificationRepository notificationRepository;
 	
-		
+	// Buscar Friends
+	public Notification buscar(Long id) {
+		Notification notification = notificationRepository.findOne(id);
+
+		if (notification == null) {
+			throw new UsersNaoEncontradoException("Nenhum Notification encontrado.");
+		}
+		return notification;
+	}
+	
+	// Verificar se Existe
+	private void verificarExistencia(Notification notification) {
+		buscar(notification.getId());
+	}
+	
+	// Classification status
+	public void status(Notification notification) {
+		verificarExistencia(notification);
+		Notification n = notificationRepository.findOne(notification.getId());
+		if (n != null) {
+			System.out.println("|Update|Notification Read: "+ " status: " + notification.getStatus());
+			notification.setDateNotify(n.getDateNotify());
+			notification.setId_fb_friends(n.getId_fb_friends());
+			notification.setImagem(n.getImagem());
+			notification.setLink(n.getLink());
+			notification.setName(n.getName());
+			notification.setText(n.getText());
+			notification.setType(n.getType());
+			notification.setUsers(n.getUsers());
+			notificationRepository.save(notification);
+		}
+		notification.setDateNotify(new Date());
+		notificationRepository.save(notification);
+	}
+	
+	// Deletar
+	public void deletar(Long id) {
+		try {
+			notificationRepository.delete(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new OptionNaoEncontradoException("Notification não pode ser Deletado.");
+		}
+	}	
 		
 	
 }
